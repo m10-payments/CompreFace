bf6caa84313dc999405e1bda746fd554f80d59e4
Ivan Kripakov <ivan.kripakov@pashapay.az>
2023-11-06 15:21:43 +0400

    provide more granular way to manage embedding cache

--- java/api/src/main/java/com/exadel/frs/core/trainservice/cache/EmbeddingCacheProvider.java
+++ java/api/src/main/java/com/exadel/frs/core/trainservice/cache/EmbeddingCacheProvider.java
@@ -1,17 +1,24 @@
 package com.exadel.frs.core.trainservice.cache;
 
+import com.exadel.frs.commonservice.entity.Embedding;
+import com.exadel.frs.commonservice.projection.EmbeddingProjection;
 import com.exadel.frs.core.trainservice.dto.CacheActionDto;
+import com.exadel.frs.core.trainservice.dto.CacheActionDto.AddEmbeddings;
+import com.exadel.frs.core.trainservice.dto.CacheActionDto.CacheAction;
+import com.exadel.frs.core.trainservice.dto.CacheActionDto.RemoveEmbeddings;
+import com.exadel.frs.core.trainservice.dto.CacheActionDto.RemoveSubjects;
+import com.exadel.frs.core.trainservice.dto.CacheActionDto.RenameSubjects;
 import com.exadel.frs.core.trainservice.service.EmbeddingService;
 import com.exadel.frs.core.trainservice.service.NotificationSenderService;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 import static com.exadel.frs.core.trainservice.system.global.Constants.SERVER_UUID;
 
@@ -34,34 +41,60 @@ public class EmbeddingCacheProvider {
                     .build();
 
     public EmbeddingCollection getOrLoad(final String apiKey) {
-
         var result = cache.getIfPresent(apiKey);
-
         if (result == null) {
             result = embeddingService.doWithEnhancedEmbeddingProjectionStream(apiKey, EmbeddingCollection::from);
-
             cache.put(apiKey, result);
-
-            notifyCacheEvent("UPDATE", apiKey);
         }
-
         return result;
     }
 
-    public void ifPresent(String apiKey, Consumer<EmbeddingCollection> consumer) {
-        Optional.ofNullable(cache.getIfPresent(apiKey))
-                .ifPresent(consumer);
+    public void removeEmbedding(String apiKey, EmbeddingProjection embedding) {
+        getOrLoad(apiKey).removeEmbedding(embedding);
+        notifyCacheEvent(
+            CacheAction.REMOVE_EMBEDDINGS,
+            apiKey,
+            new RemoveEmbeddings(Map.of(embedding.subjectName(), List.of(embedding.embeddingId())))
+        );
+    }
 
-        cache.getIfPresent(apiKey);
-        notifyCacheEvent("UPDATE", apiKey);
+    public void updateSubjectName(String apiKey, String oldSubjectName, String newSubjectName) {
+        getOrLoad(apiKey).updateSubjectName(oldSubjectName, newSubjectName);
+        notifyCacheEvent(CacheAction.RENAME_SUBJECTS, apiKey, new RenameSubjects(Map.of(oldSubjectName, newSubjectName)));
+    }
+
+    public void removeBySubjectName(String apiKey, String subjectName) {
+        getOrLoad(apiKey).removeEmbeddingsBySubjectName(subjectName);
+        notifyCacheEvent(CacheAction.REMOVE_SUBJECTS, apiKey, new RemoveSubjects(List.of(subjectName)));
+    }
+
+
+    public void addEmbedding(String apiKey, Embedding embedding) {
+        getOrLoad(apiKey).addEmbedding(embedding);
+        notifyCacheEvent(CacheAction.ADD_EMBEDDINGS, apiKey, new AddEmbeddings(List.of(embedding.getId())));
+    }
+
+    /**
+     * Method can be used to make changes in cache without sending notification.
+     * Use it carefully, because changes you do will not be visible for other compreface-api instances
+     *
+     * @param apiKey domain
+     * @param action what to do with {@link EmbeddingCollection}
+     */
+    public void exposeIfPresent(String apiKey, Consumer<EmbeddingCollection> action) {
+        action.accept(getOrLoad(apiKey));
     }
 
     public void invalidate(final String apiKey) {
         cache.invalidate(apiKey);
-        notifyCacheEvent("DELETE", apiKey);
+        notifyCacheEvent(CacheAction.INVALIDATE, apiKey, null);
     }
 
-
+    /**
+     * @deprecated
+     * See {@link com.exadel.frs.core.trainservice.service.NotificationHandler#handleUpdate(CacheActionDto)}
+     */
+    @Deprecated(forRemoval = true)
     public void receivePutOnCache(String apiKey) {
         var result = embeddingService.doWithEnhancedEmbeddingProjectionStream(apiKey, EmbeddingCollection::from);
         cache.put(apiKey, result);
@@ -71,8 +104,8 @@ public class EmbeddingCacheProvider {
         cache.invalidate(apiKey);
     }
 
-    private void notifyCacheEvent(String event, String apiKey) {
-        CacheActionDto cacheActionDto = new CacheActionDto(event, apiKey, SERVER_UUID);
+    private void notifyCacheEvent(CacheAction event, String apiKey, Object action) {
+        CacheActionDto<?> cacheActionDto = new CacheActionDto<>(event, apiKey, SERVER_UUID, action);
         notificationSenderService.notifyCacheChange(cacheActionDto);
     }
 }
